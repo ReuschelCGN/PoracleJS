584f07cc2b255e4e6c8edf687bf6c8659b11dec2
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2022-01-03 18:01:12 +0100

    Update monster.js

--- src/controllers/monster.js
+++ src/controllers/monster.js
@@ -194,9 +194,6 @@ class Monster extends Controller {
         || !(['string', 'number'].includes(typeof data.individual_defense) && (+data.individual_defense + 1))
         || !(['string', 'number'].includes(typeof data.individual_stamina) && (+data.individual_stamina + 1)))
 
-                        const sunrise = getSunrise(data.latitude, data.longitude);
-                        const sunset = getSunset(data.latitude, data.longitude);
-
 			if (data.pokestop_name) data.pokestop_name = this.escapeJsonString(data.pokestop_name)
 			data.pokestopName = data.pokestop_name
 			data.pokemonId = data.pokemon_id
@@ -240,11 +237,11 @@ class Monster extends Controller {
 			data.tthSeconds = data.disappear_time - Date.now() / 1000
 			data.tth = moment.preciseDiff(Date.now(), data.disappear_time * 1000, true)
 			const disappearTime = moment(data.disappear_time * 1000).tz(geoTz.find(data.latitude, data.longitude).toString())
+                        const sunrise = getSunrise(data.latitude, data.longitude);
+                        const sunset = getSunset(data.latitude, data.longitude);
 			data.disappearTime = disappearTime.format(this.config.locale.time)
                         //			data.nightTime = disappearTime.hour() < 8 || disappearTime.hour() >= 20
                         data.nightTime = disappearTime.hour() < sunrise() || disappearTime.hour() >= sunset()
-			data.sunrise = sunrise.format(this.config.locale.time)
-			data.sunset = sunset.format(this.config.locale.time)
 			data.confirmedTime = data.disappear_time_verified
 			data.distime = data.disappearTime // deprecated
 			data.individual_attack = data.atk // deprecated
