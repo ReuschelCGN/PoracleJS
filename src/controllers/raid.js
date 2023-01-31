60ad7bbfc187c0efc34226aa33a064139b06840b
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2023-01-31 14:22:59 +0100

    Update raid.js

--- src/controllers/raid.js
+++ src/controllers/raid.js
@@ -150,6 +150,9 @@ class Raid extends Controller {
 			if (this.config.general.rocketMadURL) {
 				data.rocketMadUrl = `${this.config.general.rocketMadURL}${!this.config.general.rocketMadURL.endsWith('/') ? '/' : ''}?lat=${data.latitude}&lon=${data.longitude}&zoom=18.0`
 			}
+			if (this.config.general.pmsfMapURL) {
+				data.pmsfMapUrl = `${this.config.general.pmsfMapURL}${!this.config.general.pmsfMapURL.endsWith('/') ? '/' : ''}?lat=${data.latitude}&lon=${data.longitude}&zoom=18&gymId=${data.gym_Id}`
+			}
 
 			data.team_id ??= 0
 			if (data.name) {
