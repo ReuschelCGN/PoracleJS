7ec0149fdeb381fa8757b6a878670f3750665e55
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2023-02-08 14:39:07 +0100

    Update pokestop.js

--- src/controllers/pokestop.js
+++ src/controllers/pokestop.js
@@ -146,7 +146,8 @@ class Invasion extends Controller {
 			// Event invasions
 			if ((data.grunt_type == 0) && (data.display_type >= 7)) {
 				data.gender = 0
-				data.gruntName = translator.translate(data.display_type && this.GameData.utilData.pokestopEvent[data.display_type] ? this.GameData.utilData.pokestopEvent[data.display_type] : '')
+				data.gruntNameEng = data.display_type && this.GameData.utilData.pokestopEvent[data.display_type] ? this.GameData.utilData.pokestopEvent[data.display_type] : ''
+				data.gruntName = translator.translate(data.gruntNameEng)
 				data.gruntType = data.display_type && this.GameData.utilData.pokestopEvent[data.display_type] ? this.GameData.utilData.pokestopEvent[data.display_type].toLowerCase() : ''
 				data.gruntRewards = ''
 			}
