f59ddf177fc324c6910f1322176941a81f452836
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2024-01-08 21:21:44 +0100

    Update postWebhooks.js

--- src/routes/postWebhooks.js
+++ src/routes/postWebhooks.js
@@ -1,4 +1,4 @@
-module.exports = async (fastify, options, next) => {
+module.exports = async (fastify, options) => {
 	fastify.post('/', options, async (req, reply) => {
 		if (fastify.config.server.ipWhitelist.length && !fastify.config.server.ipWhitelist.includes(req.ip)) return { webserver: 'unhappy', reason: `ip ${req.ip} not in whitelist` }
 		if (fastify.config.server.ipBlacklist.length && fastify.config.server.ipBlacklist.includes(req.ip)) return { webserver: 'unhappy', reason: `ip ${req.ip} in blacklist` }
@@ -8,5 +8,4 @@ module.exports = async (fastify, options, next) => {
 		fastify.hookQueue.push(...data)
 		if (!reply.sent) return { webserver: 'happy' }
 	})
-	next()
 }
