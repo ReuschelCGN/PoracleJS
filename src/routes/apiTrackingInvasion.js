59080922330f3ff388b8c55bf1e5deed2a9a618a
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2025-05-30 11:10:50 +0200

    Update apiTrackingInvasion.js

--- src/routes/apiTrackingInvasion.js
+++ src/routes/apiTrackingInvasion.js
@@ -132,14 +132,14 @@ module.exports = async (fastify, options) => {
 			if ((alreadyPresent.length + updates.length + insert.length) > 50) {
 				message = translator.translateFormat('I have made a lot of changes. See {0}{1} for details', '!', /* util.prefix, */ translator.translate('tracked'))
 			} else {
-				for (const lure of alreadyPresent) {
-					message = message.concat(translator.translate('Unchanged: '), trackedCommand.invasionRowText(fastify.config, translator, fastify.GameData, lure), '\n')
+				for (const invasion of alreadyPresent) {
+					message = message.concat(translator.translate('Unchanged: '), trackedCommand.invasionRowText(fastify.config, translator, fastify.GameData, invasion), '\n')
 				}
-				for (const lure of updates) {
-					message = message.concat(translator.translate('Updated: '), trackedCommand.invasionRowText(fastify.config, translator, fastify.GameData, lure), '\n')
+				for (const invasion of updates) {
+					message = message.concat(translator.translate('Updated: '), trackedCommand.invasionRowText(fastify.config, translator, fastify.GameData, invasion), '\n')
 				}
-				for (const lure of insert) {
-					message = message.concat(translator.translate('New: '), trackedCommand.invasionRowText(fastify.config, translator, fastify.GameData, lure), '\n')
+				for (const invasion of insert) {
+					message = message.concat(translator.translate('New: '), trackedCommand.invasionRowText(fastify.config, translator, fastify.GameData, invasion), '\n')
 				}
 			}
 
